fd607ca7d86cfc12695cf1481daabb4204bccfed
Ben Graveline <bjgraveline@gmail.com>
2017-09-19 19:25:11 -0400

    Included realistic gravity acceleration

--- main.js
+++ main.js
@@ -3,38 +3,43 @@ var el = document.getElementById("main"),
 		fullscreen: true
 	});
 
-two.appendTo(el);
+two.appendTo(el); //Attach two.js to html element
 
-var line = two.makeLine(0, two.height * 3 / 4, two.width, two.height * 3 / 4); //371
+var ground = two.height * 3 / 4; //Where the red line (ground) should be
+var ceiling = 100; //Same as radius of circle so that the top reaches the edge of the screen
+var isFalling = true; //Starts from top and falls towards ground
+var acceleration = .1;
+//Make ground
+var line = two.makeLine(0, ground, two.width, ground); 
 line.linewidth = 10;
 line.stroke = "red";
 
+//Make ball
 var ball = two.makeCircle(two.width / 2, 100, 100);
 ball.noStroke();
 ball.fill = "blue";
 
-var isFalling = true;
-
-function moveUp(){
-	ball.translation.y = ball.translation.y - 5;
+function moveUp(acceleration){
+	ball.translation.y = ball.translation.y - 1 * acceleration;
 }
 
-function moveDown(){
-	ball.translation.y = ball.translation.y + 5;
+function moveDown(acceleration){
+	ball.translation.y = ball.translation.y + 1 * acceleration;
 }
 
 two.bind("update", function(frameCount) {
-	if(ball.translation.y <= 550 && isFalling) {
-		moveDown();
-	} else if (ball.translation.y > 100 && !isFalling) {
-		moveUp();
-		console.log(isFalling);
-	} else if (ball.translation.y <= 100) {
+	if(ball.translation.y <= ground - 100 && isFalling) {
+		moveDown(acceleration);
+		acceleration = acceleration + .1;
+	} else if (ball.translation.y > ceiling && !isFalling) {
+		moveUp(acceleration);
+		acceleration = acceleration - .1;
+		//Logic for when to switch direction
+	} else if (ball.translation.y <= ceiling) {
 		isFalling = true;
-	} else if (ball.translation.y >= 550) {
+		acceleration = .1;
+	} else if (ball.translation.y > ground - 100) {
 		isFalling = false;
 	}
-
 });
-
 two.play();
